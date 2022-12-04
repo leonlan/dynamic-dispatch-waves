464b50c4cdf5f74ad9ff5ae7bafa12ffdd90ae5a
Leon Lan <30997278+leonlan@users.noreply.github.com>
2022-12-04 10:27:36 +0100

    Refactor environment (#15)

--- environment.py
+++ environment.py
@@ -12,90 +12,78 @@ Info = Dict[str, Any]
 
 class VRPEnvironment:
     """
-    Dynamic environment for VRP with Time Windows (VRPTW) where orders
-    arrive during the day An instance is created by resampling customers, time
-    windows and demands from a static VRPTW instance. Requests arrive during a
-    number of epochs. At every epoch, a solution can be submitted that
-    dispatches a number of vehicles to serve a subset of the open requests.
-    Some requests must be served during the current epoch, because the next
-    epoch will be too late to meet there time windows, taking into account
-    dispatching time and driving time. Other requests may be served, and may
-    be included in the routes, but may also be deferred to the next epoch.
-    During the final epoch, all request must be dispatched. The environment
-    has a wall clock time limit to submit solutions. To avoid timing issues,
-    submissions can be 'submitted' to the environment early, after which they
-    can still be updated until the time limit. A submitted solution is checked
-    and can be accepted or rejected depending on whether it is valid or not.
-    To move to the next epoch, the agent should explicitly call the
-    'next_epoch' function, after which the last accepted solution becomes
-    final, and new requests are sampled for the next epoch.
+    Parameters
+    ----------
+    seed
+        Random seed.
+    instance
+        The static VRP instance from which requests are sampled.
+    epoch_tlim
+        The epoch time limit.
+    max_requests_per_epoch
+        The maximum number of revealed requests per epoch.
+    dispatch_margin
+        The preparation time needed to dispatch a set of routes. That is, when
+        a set of routes are to be dispatched at epoch t, then the start time of
+        the routes is `t * epoch_duration + dispatch_margin`.
+    epoch_duration
+        The time between two consecutive epochs.
     """
 
     def __init__(
         self,
         seed: int,
         instance: Dict,
-        epoch_tlim: int,
+        epoch_tlim: float,
         max_requests_per_epoch: int = 100,
-        margin_dispatch: int = 3600,
+        dispatch_margin: int = 3600,
         epoch_duration: int = 3600,
     ):
         self.rng = np.random.default_rng(seed)
         self.instance = instance
         self.epoch_tlim = epoch_tlim
-
         self.max_requests_per_epoch = max_requests_per_epoch
-        self.margin_dispatch = margin_dispatch
+        self.dispatch_margin = dispatch_margin
         self.epoch_duration = epoch_duration
 
-        # Require reset to be called first by marking environment as done
-        self.is_done = True
+        self.is_done = True  # Requires reset to be called first
 
     def reset(self) -> Tuple[State, Info]:
         """
         Resets the environment.
         """
-        tws = self.instance["time_windows"]
+        tw = self.instance["time_windows"]
 
-        # Start epoch depends on minimum earliest customer time window
-        self.start_epoch = int(
-            max(
-                (tws[1:, 0].min() - self.margin_dispatch)
-                // self.epoch_duration,
-                0,
-            )
-        )
+        # The start and end epochs are determined by the earliest and latest
+        # opening moments of time windows, corrected by the dispatch margin.
+        earliest_open = tw[1:, 0].min() - self.dispatch_margin
+        latest_open = tw[1:, 0].max() - self.dispatch_margin
 
-        # End epoch depends on maximum earliest customer time window
-        self.end_epoch = int(
-            max(
-                (tws[1:, 0].max() - self.margin_dispatch)
-                // self.epoch_duration,
-                0,
-            )
-        )
+        self.start_epoch = int(max(earliest_open // self.epoch_duration, 0))
+        self.end_epoch = int(max(latest_open // self.epoch_duration, 0))
 
         self.current_epoch = self.start_epoch
+        self.current_time = self.current_epoch * self.epoch_duration
 
         # Initialize request array with dummy request for depot
-        self.request_id = np.array([0])
-        self.request_customer_index = np.array([0])
-        self.request_timewi = self.instance["time_windows"][0:1]
-        self.request_service_t = self.instance["service_times"][0:1]
-        self.request_demand = self.instance["demands"][0:1]
-        self.request_is_dispatched = np.array([False])
-        self.request_epoch = np.array([0])
-        self.request_must_dispatch = np.array([False])
+        self.req_idx = np.array([0])
+        self.req_customer_idx = np.array([0])
+        self.req_tw = self.instance["time_windows"][0:1]
+        self.req_service = self.instance["service_times"][0:1]
+        self.req_demand = self.instance["demands"][0:1]
+        self.req_is_dispatched = np.array([False])
+        self.req_epoch = np.array([0])
+        self.req_must_dispatch = np.array([False])
 
         self.is_done = False
         obs = self._next_observation()
 
         self.final_solutions: Dict[int, Optional[List]] = {}
         self.final_costs: Dict[int, Optional[float]] = {}
+
         self.start_time_epoch = time.time()
 
         info = {
-            # For the dynamic problem, requests will be sampled from the static instance
             "dynamic_context": self.instance,
             "start_epoch": self.start_epoch,
             "end_epoch": self.end_epoch,
@@ -108,182 +96,164 @@ class VRPEnvironment:
     def step(
         self, solution: Action
     ) -> Tuple[Optional[State], float, bool, Info]:
-        assert not self.is_done, "Environment is finished"
-
-        # Check time limit (2 seconds grace period)
-        if self._get_elapsed_time_epoch() > self.epoch_tlim + 2:
-            return self._fail_episode("Time exceeded")
-
-        # Check if solution is valid
+        """
+        Steps to the next epoch. If the submitted solution is valid, then this
+        method returns the observation of the next epoch. Otherwise, the epoch
+        is failed and the environment is done.
+        """
         try:
-            driving_duration = tools.validate_dynamic_epoch_solution(
-                self.epoch_instance, solution
-            )
-        except AssertionError as e:
-            return self._fail_episode(e)
+            self._validate_step(solution)
+        except AssertionError as error:
+            self.is_done = True
+            return (None, float("inf"), self.is_done, {"error": str(error)})
 
-        # Mark orders of current solution as dispatched
-        for route in solution:
-            # Route consists of 1 indexed requests
-            assert not self.request_is_dispatched[route].any()
-            self.request_is_dispatched[route] = True
-
-        # We must not have any undispatched orders that must be dispatched
-        assert not (
-            self.request_must_dispatch & ~self.request_is_dispatched
-        ).any()
+        cost = tools.validate_dynamic_epoch_solution(self.ep_inst, solution)
 
         self.final_solutions[self.current_epoch] = solution
-        self.final_costs[self.current_epoch] = driving_duration
+        self.final_costs[self.current_epoch] = cost
+
         self.current_epoch += 1
+        self.current_time = self.current_epoch * self.epoch_duration
         self.is_done = self.current_epoch > self.end_epoch
 
         observation = self._next_observation() if not self.is_done else None
-        reward = -driving_duration
+        reward = -cost
 
         self.start_time_epoch = time.time()
         return (observation, reward, self.is_done, {"error": None})
 
-    def _get_elapsed_time_epoch(self):
-        assert self.start_time_epoch is not None
-        return time.time() - self.start_time_epoch
+    def _validate_step(self, solution):
+        """
+        Validates if the solution was submitted on time, and whether it
+        satisfies the dynamic and static constraints.
+        """
+        assert not self.is_done, "Environment is finished"
 
-    def _fail_episode(self, error):
-        self.final_solutions[self.current_epoch] = None
-        self.final_costs[self.current_epoch] = float("inf")
-        self.is_done = True
-        return (None, float("inf"), self.is_done, {"error": str(error)})
+        # Check time limit (2 seconds grace period)
+        on_time = time.time() - self.start_time_epoch < self.epoch_tlim + 2
+        assert on_time, "Time limit exceeded"
+
+        # Check if solution is valid
+        tools.validate_dynamic_epoch_solution(self.ep_inst, solution)
+
+        # Mark orders of submitted solution as dispatched
+        for route in solution:
+            assert not self.req_is_dispatched[route].any()
+            self.req_is_dispatched[route] = True
+
+        # We must not have any undispatched orders that must be dispatched
+        undispatched = (self.req_must_dispatch & ~self.req_is_dispatched).any()
+        assert not undispatched, "Must dispatch requests not dispatched."
 
     def _next_observation(self) -> State:
-        assert not self.is_done
-        assert self.start_epoch <= self.current_epoch <= self.end_epoch
+        """
+        Returns the next observation. This consists of all requests that were
+        not dispatched during the previous epoch, and newly sampled requests.
 
-        duration_matrix = self.instance["duration_matrix"]
+        # TODO We could sample all requests at the initialization, because the
+        new observations do not depend on any state-specific information.
+        """
+        dist = self.instance["duration_matrix"]
 
-        # Sample new data
-        current_time = self.epoch_duration * self.current_epoch
-        planning_starttime = current_time + self.margin_dispatch
+        dispatch_time = self.current_time + self.dispatch_margin
 
-        # Sample uniformly
-        num_customers = len(self.instance["coords"]) - 1  # Exclude depot
+        n_customers = self.instance["is_depot"].size - 1  # Exclude depot
+        n_samples = self.max_requests_per_epoch
 
         # Sample data uniformly from customers (1 to num_customers)
-        def sample_from_customers(k=self.max_requests_per_epoch):
-            return self.rng.integers(num_customers, size=k) + 1
+        cust_idx = self.rng.integers(n_customers, size=n_samples) + 1
+        tw_idx = self.rng.integers(n_customers, size=n_samples) + 1
+        demand_idx = self.rng.integers(n_customers, size=n_samples) + 1
+        service_idx = self.rng.integers(n_customers, size=n_samples) + 1
 
-        cust_idx = sample_from_customers()
-        timewi_idx = sample_from_customers()
-        demand_idx = sample_from_customers()
-        service_t_idx = sample_from_customers()
+        new_tw = self.instance["time_windows"][tw_idx]
+        new_demand = self.instance["demands"][demand_idx]
+        new_service = self.instance["service_times"][service_idx]
 
-        new_request_timewi = self.instance["time_windows"][timewi_idx]
-        # Filter data that can no longer be delivered
-        # Time + margin for dispatch + drive time from depot should not exceed latest arrival
+        # Filter sampled requests that cannot be served in a round trip
         earliest_arrival = np.maximum(
-            planning_starttime + duration_matrix[0, cust_idx],
-            new_request_timewi[:, 0],
+            dispatch_time + dist[0, cust_idx], new_tw[:, 0]
         )
-        # Also, return at depot in time must be feasible
-        earliest_return_at_depot = (
-            earliest_arrival
-            + self.instance["service_times"][service_t_idx]
-            + duration_matrix[cust_idx, 0]
-        )
-        is_feasible = (earliest_arrival <= new_request_timewi[:, 1]) & (
-            earliest_return_at_depot <= self.instance["time_windows"][0, 1]
+        earliest_return = earliest_arrival + new_service + dist[cust_idx, 0]
+        depot_closed = self.instance["time_windows"][0, 1]
+
+        feas = (earliest_arrival <= new_tw[:, 1]) & (
+            earliest_return <= depot_closed
         )
 
-        if is_feasible.any():
-            num_new_requests = is_feasible.sum()
-            self.request_id = np.concatenate(
+        if feas.any():  # Store all new feasible requests
+            n_new_requests = feas.sum()
+
+            self.req_idx = np.concatenate(
                 (
-                    self.request_id,
-                    np.arange(num_new_requests) + len(self.request_id),
+                    self.req_idx,
+                    np.arange(n_new_requests) + len(self.req_idx),
                 )
             )
-            self.request_customer_index = np.concatenate(
-                (self.request_customer_index, cust_idx[is_feasible])
+            self.req_customer_idx = np.concatenate(
+                (self.req_customer_idx, cust_idx[feas])
             )
-            self.request_timewi = np.concatenate(
-                (self.request_timewi, new_request_timewi[is_feasible])
+            self.req_tw = np.concatenate((self.req_tw, new_tw[feas]))
+            self.req_service = np.concatenate(
+                (self.req_service, new_service[feas])
             )
-            self.request_service_t = np.concatenate(
-                (
-                    self.request_service_t,
-                    self.instance["service_times"][service_t_idx[is_feasible]],
-                )
+            self.req_demand = np.concatenate(
+                (self.req_demand, new_demand[feas])
             )
-            self.request_demand = np.concatenate(
-                (
-                    self.request_demand,
-                    self.instance["demands"][demand_idx[is_feasible]],
-                )
+            self.req_is_dispatched = np.pad(
+                self.req_is_dispatched, (0, n_new_requests), mode="constant"
             )
-            self.request_is_dispatched = np.pad(
-                self.request_is_dispatched,
-                (0, num_new_requests),
-                mode="constant",
-            )
-            self.request_epoch = np.concatenate(
-                (
-                    self.request_epoch,
-                    np.full(num_new_requests, self.current_epoch),
-                )
+            self.req_epoch = np.concatenate(
+                (self.req_epoch, np.full(n_new_requests, self.current_epoch))
             )
 
-        # Customers must dispatch this epoch if next epoch they will be too late
+        # Determine which requests are must-dispatch in the next epoch
         if self.current_epoch < self.end_epoch:
+            next_dispatch_time = dispatch_time + self.epoch_duration
+
             earliest_arrival = np.maximum(
-                planning_starttime
-                + self.epoch_duration
-                + duration_matrix[0, self.request_customer_index],
-                self.request_timewi[:, 0],
+                next_dispatch_time + dist[0, self.req_customer_idx],
+                self.req_tw[:, 0],
             )
             earliest_return_at_depot = (
                 earliest_arrival
-                + self.request_service_t
-                + duration_matrix[self.request_customer_index, 0]
+                + self.req_service
+                + dist[self.req_customer_idx, 0]
             )
-            self.request_must_dispatch = (
-                earliest_arrival > self.request_timewi[:, 1]
-            ) | (
-                earliest_return_at_depot > self.instance["time_windows"][0, 1]
+
+            self.req_must_dispatch = (earliest_arrival > self.req_tw[:, 1]) | (
+                earliest_return_at_depot > depot_closed
             )
         else:
-            self.request_must_dispatch = self.request_id > 0
+            self.req_must_dispatch = self.req_idx > 0
 
         # Return instance based on customers not yet dispatched
-        idx_undispatched = self.request_id[~self.request_is_dispatched]
-        customer_idx = self.request_customer_index[idx_undispatched]
+        undispatched = self.req_idx[~self.req_is_dispatched]
+        customer_idx = self.req_customer_idx[undispatched]
 
-        # Return a VRPTW instance with undispatched requests with two
-        # additional properties: customer_idx and request_idx
-        time_windows = self.request_timewi[idx_undispatched]
+        # Normalize TW to dispatch_time, and clip the past
+        time_windows = np.maximum(self.req_tw[undispatched] - dispatch_time, 0)
 
-        # Renormalize time to start at planning_starttime, and clip time windows in the past (so depot will start at 0)
-        time_windows = np.clip(
-            time_windows - planning_starttime, a_min=0, a_max=None
-        )
-
-        self.epoch_instance = {
+        self.ep_inst = {
             "is_depot": self.instance["is_depot"][customer_idx],
             "customer_idx": customer_idx,
-            "request_idx": idx_undispatched,
+            "request_idx": undispatched,
             "coords": self.instance["coords"][customer_idx],
-            "demands": self.request_demand[idx_undispatched],
+            "demands": self.req_demand[undispatched],
             "capacity": self.instance["capacity"],
             "time_windows": time_windows,
-            "service_times": self.request_service_t[idx_undispatched],
+            "service_times": self.req_service[undispatched],
             "duration_matrix": self.instance["duration_matrix"][
                 np.ix_(customer_idx, customer_idx)
             ],
-            "must_dispatch": self.request_must_dispatch[idx_undispatched],
+            "must_dispatch": self.req_must_dispatch[undispatched],
         }
+
         return {
             "current_epoch": self.current_epoch,
-            "current_time": current_time,
-            "planning_starttime": planning_starttime,
-            "epoch_instance": self.epoch_instance,
+            "current_time": self.current_time,
+            "dispatch_time": dispatch_time,
+            "epoch_instance": self.ep_inst,
         }
 
     def get_hindsight_problem(self) -> State:
@@ -292,24 +262,24 @@ class VRPEnvironment:
         'hindsight problem', i.e., as if we had future information about all the
         requests. This includes the release times of the requests.
         """
-        customer_idx = self.request_customer_index
+        customer_idx = self.req_customer_idx
 
         # Release times indicate that a route containing this request cannot
         # dispatch before this time. This includes the margin time for dispatch
         release_times = (
-            self.epoch_duration * self.request_epoch + self.margin_dispatch
+            self.epoch_duration * self.req_epoch + self.dispatch_margin
         )
         release_times[self.instance["is_depot"][customer_idx]] = 0
 
         return {
             "is_depot": self.instance["is_depot"][customer_idx],
             "customer_idx": customer_idx,
-            "request_idx": self.request_id,
+            "request_idx": self.req_idx,
             "coords": self.instance["coords"][customer_idx],
-            "demands": self.request_demand,
+            "demands": self.req_demand,
             "capacity": self.instance["capacity"],
-            "time_windows": self.request_timewi,
-            "service_times": self.request_service_t,
+            "time_windows": self.req_tw,
+            "service_times": self.req_service,
             "duration_matrix": self.instance["duration_matrix"][
                 np.ix_(customer_idx, customer_idx)
             ],

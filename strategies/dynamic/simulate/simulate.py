2758c7c0e4a52d18289089e9c7030a2ade624bc0
l-lan <l.lan@vu.nl>
2023-01-23 17:43:09 +0100

    Refactor making initial solutions

--- strategies/dynamic/simulate/simulate.py
+++ strategies/dynamic/simulate/simulate.py
@@ -43,35 +43,15 @@ def simulate(
     to_postpone = np.zeros(ep_size, dtype=bool)
 
     for cycle_idx in range(n_cycles):
-        # Get the threshold belonging to the current epoch, or the last one
+        # Get the threshold belonging to the current cycle_idx, or the last one
         # available if there are more epochs than thresholds.
         threshold_idx = min(cycle_idx, len(postpone_thresholds) - 1)
         postpone_threshold = postpone_thresholds[threshold_idx]
         dispatch_threshold = dispatch_thresholds[threshold_idx]
 
-        # Compute the initial solution for to dispatch requests. These form the
-        # "backbone" of the simulation solutions.
-        to_dispatch_instance = filter_instance(
-            ep_inst, to_dispatch | ep_inst["is_depot"]
+        disp_init = solve_dispatch_inst(
+            ep_inst, to_dispatch, node_ops, route_ops, crossover_ops
         )
-        to_dispatch_res = hgs(
-            to_dispatch_instance,
-            hgspy.Config(),
-            [getattr(hgspy.operators, op) for op in node_ops],
-            [getattr(hgspy.operators, op) for op in route_ops],
-            [getattr(hgspy.crossover, op) for op in crossover_ops],
-            hgspy.stop.MaxRuntime(5),
-        )
-
-        # We need to map the solution back to the epoch instance indices.
-        idx2cust = {
-            idx: cust for idx, cust in enumerate(to_dispatch.nonzero()[0], 1)
-        }
-        to_dispatch_init = [
-            [idx2cust[idx] for idx in route]
-            for route in to_dispatch_res.get_best_found().get_routes()
-            if route
-        ]
 
         for _ in range(n_simulations):
             sim_inst = simulate_instance(
@@ -83,13 +63,6 @@ def simulate(
                 to_dispatch=to_dispatch,
                 to_postpone=to_postpone,
             )
-            # We need to add the "non-initialized" requests to make a complete
-            # initial solution.
-            sim_init = [
-                [req]
-                for req in range(1, sim_inst["is_depot"].size)
-                if req not in to_dispatch.nonzero()[0]
-            ]
 
             res = hgs(
                 sim_inst,
@@ -98,7 +71,7 @@ def simulate(
                 [getattr(hgspy.operators, op) for op in route_ops],
                 [getattr(hgspy.crossover, op) for op in crossover_ops],
                 hgspy.stop.MaxRuntime(single_sim_tlim),
-                initial_solutions=(to_dispatch_init + sim_init,),
+                initial_solutions=(make_sim_init(sim_inst, disp_init),),
             )
 
             best = res.get_best_found()
@@ -137,9 +110,44 @@ def simulate(
 
         dispatch_count *= 0  # reset dispatch count
 
-    # TODO This should become a parameter: we can also dispatch only those
-    # requests that have been marked `to_dispatch`.
-    # Dispatch all requests that are not marked `to_postpone`
     to_dispatch = ep_inst["is_depot"] | ep_inst["must_dispatch"] | to_dispatch
-
     return filter_instance(ep_inst, to_dispatch)
+
+
+def solve_dispatch_inst(
+    ep_inst, to_dispatch, node_ops, route_ops, crossover_ops
+):
+    """
+    Solves the instance formed by dispatched requests. The solution indices
+    are mapped back to the indices of the epoch instance.
+    """
+    inst = filter_instance(ep_inst, to_dispatch | ep_inst["is_depot"])
+    res = hgs(
+        inst,
+        hgspy.Config(),
+        [getattr(hgspy.operators, op) for op in node_ops],
+        [getattr(hgspy.operators, op) for op in route_ops],
+        [getattr(hgspy.crossover, op) for op in crossover_ops],
+        hgspy.stop.MaxRuntime(5),  # TODO Make param
+    )
+
+    # Map the new indices back to the epoch instance indices.
+    ep_idcs = to_dispatch.nonzero()[0]
+    idx2ep = {idx: cust for idx, cust in enumerate(ep_idcs, 1)}
+
+    sol = res.get_best_found().get_routes()
+    disp_init = [[idx2ep[idx] for idx in route] for route in sol if route]
+    return disp_init
+
+
+def make_sim_init(sim_inst, disp_init):
+    """
+    Makes the initial solution for the simulation instance. The HGS solver only
+    accepts initial solutions that contain all requests.
+    """
+    n_reqs = sim_inst["is_depot"].size
+    disp = set(r for route in disp_init for r in route)
+
+    # Make roundtrip routes for requests not marked dispatch
+    roundtrips = [[req] for req in range(1, n_reqs) if req not in disp]
+    return disp_init + roundtrips

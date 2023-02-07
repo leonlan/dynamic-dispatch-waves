1a3e239c7ee36050221a3c392e4d7cd7a526f247
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-02-07 11:21:45 +0100

    Count as dispatch if route cannot be postponed (#38)

--- strategies/dynamic/simulate/consensus/utils.py
+++ strategies/dynamic/simulate/consensus/utils.py
@@ -1,16 +1,42 @@
-def is_dispatched(route, to_dispatch):
+def is_dispatched(instance, route, to_dispatch):
     """
-    Returns true if the route contains requests that are marked `to_dispatch`.
+    Returns true if the route contains requests that are marked `to_dispatch`,
+    or if the route, consisting of epoch requests, cannot be postponed to the
+    next epoch.
     """
-    return any(to_dispatch[idx] for idx in route if idx < to_dispatch.size)
+    n_reqs = to_dispatch.size
+
+    is_to_dispatch = any(to_dispatch[idx] for idx in route if idx < n_reqs)
+    only_ep_reqs = all(idx < n_reqs for idx in route)
+    cannot_postpone = not can_postpone_route(instance, route)
+
+    return is_to_dispatch or (only_ep_reqs and cannot_postpone)
 
 
-def is_postponed(route, to_postpone):
+def can_postpone_route(instance, route):
     """
-    Returns true if the route contains requests that are marked `to_postpone`
-    or sampled requests.
+    Checks if the route can be postponed to the next epoch, i.e., the route
+    remains feasible if it starts one epoch duration later.
     """
-    ep_size = to_postpone.size
-    return any(to_postpone[idx] for idx in route if idx < ep_size) or any(
-        idx >= ep_size for idx in route
+    tour = [0] + route + [0]
+    tws = instance["time_windows"]
+    dist = instance["duration_matrix"]
+    service = instance["service_times"]
+
+    current_time = (
+        3600  # TODO This should be the epoch duration from the environment
     )
+
+    for idx in range(len(tour) - 1):
+        pred, succ = tour[idx], tour[idx + 1]
+
+        earliest_arrival, latest_arrival = tws[succ]
+        arrival_time = current_time + dist[pred, succ]
+        current_time = max(arrival_time, earliest_arrival)
+
+        if current_time <= latest_arrival:
+            return False
+
+        current_time += service[succ]
+
+    return True

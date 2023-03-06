6c963f93eb249916178b9bf218787c75f1e25f94
l-lan <l.lan@vu.nl>
2023-03-06 20:11:20 +0100

    Use `vrplib` to read instances

--- tools/io.py
+++ tools/io.py
@@ -1,4 +1,5 @@
 import numpy as np
+import vrplib
 
 
 def _readlines(filename):
@@ -78,7 +79,7 @@ def read_vrptw_solution(filename, return_extra=False):
     return solution
 
 
-def read_vrplib(filename, rounded=True):
+def read_vrplib(filename):
     """
     Read a VRPLIB instance from file and return an `instance` dict, containing
     - 'is_depot': boolean np.array. True for depot; False otherwise.
@@ -89,116 +90,24 @@ def read_vrplib(filename, rounded=True):
     - 'service_times': np.array of service times at each client (incl. depot)
     - 'duration_matrix': distance matrix between clients (incl. depot)
     """
-    loc = []
-    demand = []
-    mode = ""
-    capacity = None
-    edge_weight_type = None
-    edge_weight_format = None
-    duration_matrix = []
-    service_t = []
-    timewi = []
-    release_times = []
-    latest_dispatch = []
-    with open(filename, "r") as f:
+    instance = vrplib.read_instance(filename)
+    n_locations = instance["dimension"]
+    horizon = instance["time_window"][0][1]  # depot latest tw
 
-        for line in f:
-            line = line.strip(" \t\n")
-            if line == "":
-                continue
-            elif line.startswith("CAPACITY"):
-                capacity = int(line.split(" : ")[1])
-            elif line.startswith("EDGE_WEIGHT_TYPE"):
-                edge_weight_type = line.split(" : ")[1]
-            elif line.startswith("EDGE_WEIGHT_FORMAT"):
-                edge_weight_format = line.split(" : ")[1]
-            elif line == "NODE_COORD_SECTION":
-                mode = "coord"
-            elif line == "DEMAND_SECTION":
-                mode = "demand"
-            elif line == "DEPOT_SECTION":
-                mode = "depot"
-            elif line == "EDGE_WEIGHT_SECTION":
-                mode = "edge_weights"
-                assert edge_weight_type == "EXPLICIT"
-                assert edge_weight_format == "FULL_MATRIX"
-            elif line == "TIME_WINDOW_SECTION":
-                mode = "time_windows"
-            elif line == "SERVICE_TIME_SECTION":
-                mode = "service_t"
-            elif line == "RELEASE_TIME_SECTION":
-                mode = "release_time"
-            elif line == "LATEST_DISPATCH_SECTION":
-                mode = "latest_dispatch"
-            elif line == "EOF":
-                break
-            elif mode == "coord":
-                (
-                    node,
-                    x,
-                    y,
-                ) = (
-                    line.split()
-                )  # Split by whitespace or \t, skip duplicate whitespace
-                node = int(node)
-                x, y = (int(x), int(y)) if rounded else (float(x), float(y))
-
-                if node == 1:
-                    depot = (x, y)
-                else:
-                    assert (
-                        node == len(loc) + 2
-                    )  # 1 is depot, 2 is 0th location
-                    loc.append((x, y))
-            elif mode == "demand":
-                node, d = [int(v) for v in line.split()]
-                if node == 1:
-                    assert d == 0
-                demand.append(d)
-            elif mode == "edge_weights":
-                duration_matrix.append(
-                    list(map(int if rounded else float, line.split()))
-                )
-            elif mode == "service_t":
-                node, t = line.split()
-                node = int(node)
-                t = int(t) if rounded else float(t)
-                if node == 1:
-                    assert t == 0
-                assert node == len(service_t) + 1
-                service_t.append(t)
-            elif mode == "time_windows":
-                node, l, u = line.split()
-                node = int(node)
-                l, u = (int(l), int(u)) if rounded else (float(l), float(u))
-                assert node == len(timewi) + 1
-                timewi.append([l, u])
-            elif mode == "release_time":
-                node, release_time = line.split()
-                release_time = int(release_time)
-                release_times.append(release_time)
-            elif mode == "latest_dispatch":
-                node, dispatch_time = line.split()
-                dispatch_time = int(dispatch_time)
-                latest_dispatch.append(dispatch_time)
-
-    horizon = timewi[0][1]  # time horizon, i.e., depot latest tw
     return {
-        "is_depot": np.array([1] + [0] * len(loc), dtype=bool),
-        "coords": np.array([depot] + loc),
-        "demands": np.array(demand),
-        "capacity": capacity,
-        "time_windows": np.array(timewi),
-        "service_times": np.array(service_t),
-        "duration_matrix": np.array(duration_matrix)
-        if len(duration_matrix) > 0
-        else None,
-        "release_times": np.array(release_times)
-        if release_times
-        else np.zeros(len(loc) + 1, dtype=int),
-        "latest_dispatch": np.array(latest_dispatch)
-        if latest_dispatch
-        else np.ones(len(loc) + 1, dtype=int) * horizon,
+        "is_depot": np.array([1] + [0] * (n_locations - 1), dtype=bool),
+        "coords": instance["node_coord"],
+        "demands": instance["demand"],
+        "capacity": instance["capacity"],
+        "time_windows": instance["time_window"],
+        "service_times": instance["service_time"],
+        "duration_matrix": instance["edge_weight"],
+        "release_times": instance["release_time"]
+        if "release_time" in instance
+        else np.zeros(n_locations, dtype=int),
+        "latest_dispatch": instance["latest_dispatch"]
+        if "latest_dispatch" in instance
+        else np.ones(n_locations, dtype=int) * horizon,
     }
 
 

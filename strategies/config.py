5ba1077799ce90aa1605bdb09cb81a61dd11239a
Leon Lan <l.lan@vu.nl>
2023-07-09 20:34:08 +0200

    Get rid of HGS-VRPTW

--- strategies/config.py
+++ strategies/config.py
@@ -2,29 +2,8 @@ from __future__ import annotations
 
 import tomli
 
-import hgspy
-
 
 class _SubConfig(dict):
-    def node_ops(self):
-        return [
-            getattr(hgspy.operators, op) for op in self.get("node_ops", [])
-        ]
-
-    def route_ops(self):
-        return [
-            getattr(hgspy.operators, op) for op in self.get("route_ops", [])
-        ]
-
-    def crossover_ops(self):
-        return [
-            getattr(hgspy.crossover, op)
-            for op in self.get("crossover_ops", [])
-        ]
-
-    def solver_params(self):
-        return self.get("params", {})
-
     def strategy_params(self):
         return self.get("strategy_params", {})
 
@@ -36,7 +15,6 @@ class _SubConfig(dict):
 
 class Config:
     def __init__(self, **kwargs):
-        self._static = _SubConfig(**kwargs.get("static", {}))
         self._dynamic = _SubConfig(**kwargs.get("dynamic", {}))
 
     @classmethod
@@ -44,9 +22,6 @@ class Config:
         with open(where, "rb") as fh:
             data = tomli.load(fh)
             return cls(**data)
-
-    def static(self) -> _SubConfig:
-        return self._static
 
     def dynamic(self) -> _SubConfig:
         return self._dynamic

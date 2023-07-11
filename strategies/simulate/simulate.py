dd1648371205c4494a61bd7cee3fb21e20802b77
Leon Lan <l.lan@vu.nl>
2023-07-11 22:43:19 +0200

    Custom PyVRP solver for scenario instaces
    This makes the current codebase almost as performant as the one
    from the initial submission.

--- strategies/simulate/simulate.py
+++ strategies/simulate/simulate.py
@@ -1,5 +1,26 @@
 import numpy as np
-from pyvrp import Model
+import pyvrp
+import pyvrp.search
+from pyvrp import (
+    GeneticAlgorithm,
+    GeneticAlgorithmParams,
+    PenaltyManager,
+    PenaltyParams,
+    Population,
+    PopulationParams,
+    Solution,
+    XorShift128,
+)
+from pyvrp.crossover import selective_route_exchange as srex
+from pyvrp.diversity import broken_pairs_distance as bpd
+from pyvrp.search import (
+    Exchange10,
+    Exchange11,
+    LocalSearch,
+    NeighbourhoodParams,
+    TwoOpt,
+    compute_neighbours,
+)
 from pyvrp.stop import MaxRuntime
 
 from utils import instance2data
@@ -55,9 +76,7 @@ def simulate(
                 to_postpone,
             )
 
-            # TODO make this as a custom solver
-            model = Model.from_data(instance2data(sim_inst))
-            res = model.solve(MaxRuntime(single_sim_tlim), seed=42)
+            res = _scenario_solver(sim_inst, MaxRuntime(single_sim_tlim), 42)
             sim_sol = [route.visits() for route in res.best.get_routes()]
 
             scenarios.append((sim_inst, sim_sol))
@@ -85,3 +104,43 @@ def simulate(
         raise ValueError("Final dispatch action unknown.")
 
     return filter_instance(ep_inst, ep_inst["is_depot"] | selected)
+
+
+def _scenario_solver(
+    instance: dict, stop: pyvrp.stop.StoppingCriterion, seed: int
+):
+    gen_params = GeneticAlgorithmParams(
+        repair_probability=0, intensify_probability=0, intensify_on_best=False
+    )
+    pen_params = PenaltyParams(
+        init_time_warp_penalty=14,
+        repair_booster=12,
+        num_registrations_between_penalty_updates=1,
+        penalty_increase=2,
+        penalty_decrease=0.34,
+        target_feasible=0.19,
+    )
+    pop_params = PopulationParams(min_pop_size=3, generation_size=8)
+    nb_params = NeighbourhoodParams(
+        weight_wait_time=5, weight_time_warp=18, nb_granular=16
+    )
+
+    data = instance2data(instance)
+    rng = XorShift128(seed=seed)
+    pen_manager = PenaltyManager(pen_params)
+    pop = Population(bpd, params=pop_params)
+
+    neighbours = compute_neighbours(data, nb_params)
+    ls = LocalSearch(data, rng, neighbours)
+
+    node_ops = [Exchange10, Exchange11, TwoOpt]
+    for node_op in node_ops:
+        ls.add_node_operator(node_op(data))
+
+    init = [
+        Solution.make_random(data, rng) for _ in range(pop_params.min_pop_size)
+    ]
+    algo = GeneticAlgorithm(
+        data, pen_manager, rng, pop, ls, srex, init, gen_params
+    )
+    return algo.run(stop)

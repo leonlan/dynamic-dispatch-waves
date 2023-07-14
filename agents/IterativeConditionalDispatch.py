8920aeda4173c9bca4a352f9715af7cd87140057
Leon Lan <l.lan@vu.nl>
2023-07-14 12:05:30 +0200

    Add some comments about ICD sampling instance

--- agents/IterativeConditionalDispatch.py
+++ agents/IterativeConditionalDispatch.py
@@ -121,32 +121,35 @@ class IterativeConditionalDispatch:
             A boolean array where True mean that the corresponding request must be
             postponed.
         """
+        # Parameters
         current_epoch = obs["current_epoch"]
         next_epoch = current_epoch + 1
         epochs_left = info["end_epoch"] - current_epoch
         max_lookahead = min(self.num_lookahead, epochs_left)
+        max_requests_per_epoch = info["max_requests_per_epoch"]
 
-        # Parameters
         static_inst = info["dynamic_context"]
         epoch_duration = info["epoch_duration"]
         ep_inst = obs["epoch_instance"]
         dispatch_time = obs["dispatch_time"]
-        dist = static_inst["duration_matrix"]
-        max_requests_per_epoch = info["max_requests_per_epoch"]
 
-        # Simulation instance
+        # Scenario instance fields
         req_customer_idx = ep_inst["customer_idx"]
         req_idx = ep_inst["request_idx"]
         req_demand = ep_inst["demands"]
         req_service = ep_inst["service_times"]
         req_tw = ep_inst["time_windows"]
 
-        # Conditional dispatching
+        # Conditional dispatching: alter release and dispatch times of requests
+        # that have already been marked in previous iterations.
         horizon = req_tw[0][1]
-        req_release = to_postpone * epoch_duration
         req_dispatch = np.where(to_dispatch, 0, horizon)
+        req_release = to_postpone * epoch_duration
 
         for epoch_idx in range(next_epoch, next_epoch + max_lookahead):
+            # Samples new requests for the next epoch. The sampled requests
+            # attributes are sampled from the static instance. Time-sensitive
+            # attributes will be normalized later.
             new = sample_epoch_requests(
                 self.rng,
                 static_inst,
@@ -154,35 +157,36 @@ class IterativeConditionalDispatch:
                 (epoch_idx + 1) * epoch_duration,  # next epoch dispatch time
                 max_requests_per_epoch,
             )
-            n_new_reqs = new["customer_idx"].size
+            num_new_reqs = new["customer_idx"].size
 
-            # Concatenate the new feasible requests to the epoch instance
+            # Concatenate the new requests to the current instance requests
             req_customer_idx = np.concatenate(
                 (req_customer_idx, new["customer_idx"])
             )
 
-            # Simulated request indices are always negative (so we can identify them)
-            sim_req_idx = -(np.arange(n_new_reqs) + 1) - len(req_idx)
-            req_idx = np.concatenate((ep_inst["request_idx"], sim_req_idx))
+            # Sampled request indices are negative so we can identify them
+            new_req_idx = -(np.arange(num_new_reqs) + 1) - len(req_idx)
+            req_idx = np.concatenate((ep_inst["request_idx"], new_req_idx))
 
             req_demand = np.concatenate((req_demand, new["demands"]))
             req_service = np.concatenate((req_service, new["service_times"]))
 
-            # Normalize TW and release to dispatch time, and clip the past
-            new["time_windows"] = np.maximum(
-                new["time_windows"] - dispatch_time, 0
-            )
+            # Normalize TW to start at dispatch time, and clip the past
+            new["time_windows"] -= dispatch_time
+            new["time_windows"] = np.maximum(new["time_windows"], 0)
             req_tw = np.concatenate((req_tw, new["time_windows"]))
 
-            new["release_times"] = np.maximum(
-                new["release_times"] - dispatch_time, 0
-            )
+            # Also normalize release time and clip the past
+            new["release_times"] -= dispatch_time
+            new["release_times"] = np.maximum(new["release_times"], 0)
             req_release = np.concatenate((req_release, new["release_times"]))
 
             # Default dispatch time is the time horizon.
             req_dispatch = np.concatenate(
-                (req_dispatch, np.full(n_new_reqs, horizon))
+                (req_dispatch, np.full(num_new_reqs, horizon))
             )
+
+        dist = static_inst["duration_matrix"]
 
         return {
             "is_depot": static_inst["is_depot"][req_customer_idx],

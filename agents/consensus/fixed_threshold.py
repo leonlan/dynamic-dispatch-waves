01582ff2be017f548cc7b7ceddac62ee11a2c6da
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-07-15 14:05:36 +0200

    Refactor consensus functions (#69)

--- agents/consensus/fixed_threshold.py
+++ agents/consensus/fixed_threshold.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from .utils import (
     select_dispatch_on_threshold,
     select_postpone_on_threshold,
@@ -6,39 +8,47 @@ from .utils import (
 
 
 def fixed_threshold(
-    cycle_idx,
-    scenarios,
-    old_dispatch,
-    old_postpone,
-    dispatch_thresholds,
-    postpone_thresholds,
+    iteration_idx: int,
+    scenarios: list[tuple[dict, list[list[int]]]],
+    old_dispatch: np.ndarray,
+    old_postpone: np.ndarray,
+    dispatch_thresholds: list[float],
+    postpone_thresholds: list[float],
     **kwargs
-):
+) -> tuple[np.ndarray, np.ndarray]:
     """
-    Uses user-specified dispatch and postpone thresholds to mark requests
-    as dispatched or postponed.
+    Uses two thresholds to mark requests as either dispatched or postponed
+    based on the dispatching frequency in scenario runs.
+
+    Parameters
+    ----------
+    iteration_idx
+        The current iteration index.
+    scenarios
+        The list of instances and solutions, one for each scenario.
+    old_dispatch
+        List of dispatch actions from the previous iteration.
+    old_postpone
+        List of postpone actions from the previous iteration.
+    dispatch_thresholds
+        List of dispatch thresholds for each iteration.
+    postpone_thresholds
+        List of postpone thresholds for each iteration.
     """
-    # Get the threshold belonging to the current cycle, or the last one
-    # available if there are more cycles than thresholds.
-    disp_thresh_idx = min(cycle_idx, len(dispatch_thresholds) - 1)
+    # Get the threshold belonging to the current iteration, or the last one
+    # available if there are more iterations than thresholds.
+    disp_thresh_idx = min(iteration_idx, len(dispatch_thresholds) - 1)
     dispatch_threshold = dispatch_thresholds[disp_thresh_idx]
 
-    post_thresh_idx = min(cycle_idx, len(postpone_thresholds) - 1)
+    post_thresh_idx = min(iteration_idx, len(postpone_thresholds) - 1)
     postpone_threshold = postpone_thresholds[post_thresh_idx]
 
-    if dispatch_threshold <= 1:
-        new_dispatch = select_dispatch_on_threshold(
-            scenarios, old_dispatch, old_postpone, dispatch_threshold
-        )
-    else:  # dispatch threshold too high, copy old action
-        new_dispatch = old_dispatch.copy()
-
-    if postpone_threshold <= 1:
-        new_postpone = select_postpone_on_threshold(
-            scenarios, old_dispatch, old_postpone, postpone_threshold
-        )
-    else:  # dispatch threshold too high, copy old action
-        new_postpone = old_postpone.copy()
+    new_dispatch = select_dispatch_on_threshold(
+        scenarios, old_dispatch, old_postpone, dispatch_threshold
+    )
+    new_postpone = select_postpone_on_threshold(
+        scenarios, old_dispatch, old_postpone, postpone_threshold
+    )
 
     verify_action(old_dispatch, old_postpone, new_dispatch, new_postpone)
 

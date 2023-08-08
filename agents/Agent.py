a9cfa21ee0727f64b710e1fbb8646c60ba0f09c4
Leon Lan <l.lan@vu.nl>
2023-08-07 21:11:38 -0700

    Update Agent protocol

--- agents/Agent.py
+++ agents/Agent.py
@@ -4,10 +4,23 @@ import numpy as np
 
 
 class Agent(Protocol):
-    def __init__(self, seed, **kwargs):
-        ...
+    """
+    Protocol describing an agent.
+    """
 
-    def act(self, static_info: dict, observation: dict) -> np.ndarray:
+    def act(self, static_info: dict, observation: dict) -> list[list[int]]:
         """
-        Returns a dispatch action for the given static info and observation.
+        Returns a routing solution for the current epoch.
+
+        Parameters
+        ----------
+        static_info
+            The static information about the environment.
+        observation
+            The observed epoch state.
+
+        Returns
+        -------
+        list[list[int]]
+            The epoch action.
         """

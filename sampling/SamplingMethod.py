bc856f7b9408a99654fc55875f6e81becbb278d7
Leon Lan <l.lan@vu.nl>
2023-08-29 14:23:50 +0200

    Document SamplingMethod

--- sampling/SamplingMethod.py
+++ sampling/SamplingMethod.py
@@ -4,6 +4,10 @@ from numpy.random import Generator
 
 
 class SamplingMethod(Protocol):
+    """
+    Protocol for sampling methods.
+    """
+
     def __call__(
         self,
         rng: Generator,
@@ -14,4 +18,29 @@ class SamplingMethod(Protocol):
         num_requests: int,
         **kwargs,
     ) -> dict:
-        ...
+        """
+        Samples a set of requests from a base instance.
+
+        Parameters
+        ----------
+        rng
+            Random number generator.
+        instance
+            Static base instance.
+        current_time
+            Current epoch time.
+        departure_time
+            Departure time of the vehicle (i.e., the request release time).
+        epoch_duration
+            Duration of the epoch.
+        num_requests
+            Number of requests to sample.
+        kwargs
+            Additional keyword arguments.
+
+        Returns
+        -------
+        dict
+            Data of sampled requests, including the customer indices, time windows,
+            demands, service times, and release times.
+        """

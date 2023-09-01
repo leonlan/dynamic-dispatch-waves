0b245840a2bfc468d958c0daaf817b8e8ab4b388
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-09-01 16:36:59 +0200

    Rolling horizon policy (#80)

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,10 +1,12 @@
 from .Agent import Agent
 from .IterativeConditionalDispatch import IterativeConditionalDispatch
 from .RandomDispatch import GreedyDispatch, LazyDispatch, UniformDispatch
+from .RollingHorizon import RollingHorizon
 
 AGENTS = {
     "greedy": GreedyDispatch,
     "lazy": LazyDispatch,
     "uniform": UniformDispatch,
     "icd": IterativeConditionalDispatch,
+    "rolling_horizon": RollingHorizon,
 }

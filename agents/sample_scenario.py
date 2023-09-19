399a8b3fe5a4677a3a84eeb22cbd865c43952420
Leon Lan <l.lan@vu.nl>
2023-09-19 08:45:23 +0200

    Make sample scenario TWs consistent

--- agents/sample_scenario.py
+++ agents/sample_scenario.py
@@ -63,7 +63,7 @@ def sample_scenario(
     req_dispatch = np.where(to_dispatch, departure_time, horizon)
 
     num_vehicles = ep_inst.num_vehicles
-    shift_tw_early = ep_inst.num_vehicles * [departure_time]
+    shift_tw_early = [departure_time] * ep_inst.num_vehicles
 
     for epoch in range(next_epoch, next_epoch + max_lookahead):
         epoch_start = epoch * epoch_duration
@@ -99,7 +99,7 @@ def sample_scenario(
         if info.num_vehicles_per_epoch is None:
             # No restriction means that we don't have to care about the TWs.
             num_vehicles += ep_inst.num_vehicles
-            shift_tw_early.extend([0] * num_vehicles)
+            shift_tw_early.extend([departure_time] * num_vehicles)
         else:
             num_vehicles += info.num_vehicles_per_epoch[epoch]
             shift_tw_early.extend([epoch_depart] * num_vehicles)

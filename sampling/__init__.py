50a49860dac5f4a7418e4622a019aa9c9d351c2d
Leon Lan <l.lan@vu.nl>
2023-09-19 21:33:57 +0200

    Fix custom time window keywords

--- sampling/__init__.py
+++ sampling/__init__.py
@@ -1,11 +1,11 @@
 from functools import partial
 
-from .custom_time_windows import custom_time_windows
+from .custom_time_windows import custom_time_windows as custom_tw
 from .euro_neurips import euro_neurips
 from .SamplingMethod import SamplingMethod
 
 SAMPLING_METHODS: dict[str, SamplingMethod] = {
-    f"{name}{tw_width}": partial(custom_time_windows, tw_type, tw_width)
+    f"{name}{tw_width}": partial(custom_tw, tw_type=tw_type, tw_width=tw_width)
     for name, tw_type in [("DL", "deadlines"), ("TW", "time_windows")]
     for tw_width in range(1, 9)
 }
